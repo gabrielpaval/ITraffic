df5a0d7869378857025e089b12fce42abedea523
gabrielpaval <dan.paval@student.usv.ro>
2022-03-23 15:48:34 +0200

    Template proiect

---
+++ src/main/java/ro/proiect/iTraffic/ITrafficApplication.java
@@ -0,0 +1,13 @@
+package ro.proiect.iTraffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITrafficApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/proiect/iTraffic/ITrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.proiect.iTraffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITrafficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
